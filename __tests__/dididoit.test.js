447acff1066baf4529dc007701a1f6aff674f93a
Frank Hollywood <github@srvsync.com>
2022-01-27 08:44:02 -0800

    Dididoit is complete

--- __tests__/dididoit.test.js
+++ __tests__/dididoit.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Dididoit = require('../lib/models/Dididoit.js');
 
 describe('backend routes', () => {
   beforeEach(() => {
@@ -12,7 +13,7 @@ describe('backend routes', () => {
     pool.end();
   });
 
-  it.skip('should create a done it or not', async () => {
+  it('should create a did it or not', async () => {
     const res = await request(app).post('/api/v1/dididoit').send({
       dididoit: 'start a big fire',
       isToBlame: false,
@@ -23,5 +24,52 @@ describe('backend routes', () => {
       dididoit: 'start a big fire',
       isToBlame: false,
     });
+  });
+
+  it('should get a dididoit by id', async () => {
+    const dididoit = await Dididoit.insert({
+      dididoit: 'Eat a whole turkey by myself',
+      isToBlame: false,
+    });
+    const res = await request(app).get('/api/v1/dididoit/1');
+    expect(res.body).toEqual(dididoit);
+  });
+
+  it('should list all the Done its', async () => {
+    const dididoit = await Dididoit.insert({
+      dididoit: 'Start an earthquake',
+      isToBlame: false,
+    });
+    const dididoit2 = await Dididoit.insert({
+      dididoit: 'Wear dirty socks',
+      isToBlame: false,
+    });
+    const res = await request(app).get('/api/v1/dididoit');
+    expect(res.body).toEqual([dididoit, dididoit2]);
+  });
+
+  it('should update projector by id', async () => {
+    const dididoit = await Dididoit.insert({
+      dididoit: 'cause someone to die.',
+      isToBlame: true,
+    });
+    const res = await request(app)
+      .patch('/api/v1/dididoit/1')
+      .send({ isToBlame: false });
+
+    expect(res.body).toEqual({
+      ...dididoit,
+      isToBlame: false,
+    });
+  });
+
+  it('should delete a projector by id', async () => {
+    const noDididoit = await Dididoit.insert({
+      dididoit: 'I have children',
+      isToBlame: false,
+    });
+    const res = await request(app).delete(`/api/v1/dididoit/${noDididoit.id}`);
+    expect(res.body).toEqual(noDididoit);
+    expect(await Dididoit.getById(noDididoit.id)).toBeNull();
   });
 });
